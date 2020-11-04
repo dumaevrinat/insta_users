ba795c1e5c5d9773367df971e27b53941a580842
dumaevrinat <dumaevrinat@gmail.com>
2020-11-04 21:51:14 +0000

    Updates

---
+++ precache-manifest.7520e70bb0c2930130f0b0db878eacfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3d2e272761f5dcd1374e45b0ba9c66",
+    "url": "/insta_users/index.html"
+  },
+  {
+    "revision": "3387a7478f226b2c906f",
+    "url": "/insta_users/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "112f44ceeaa7fa22c048",
+    "url": "/insta_users/static/js/2.4876d176.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/insta_users/static/js/2.4876d176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3387a7478f226b2c906f",
+    "url": "/insta_users/static/js/main.13668e12.chunk.js"
+  },
+  {
+    "revision": "33e45fcc1ba9809c1048",
+    "url": "/insta_users/static/js/runtime-main.aa34f739.js"
+  }
+]);
